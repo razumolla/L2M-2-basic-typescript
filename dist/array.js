03fde66e4ceb0d6923c523125baef3e8f22906b0
razumolla <rajuahmed9344@gmail.com>
2023-05-01 16:53:04 +0600

    function in ts

--- dist/array.js
+++ dist/array.js
@@ -3,8 +3,17 @@
  * when we declare any type of Arry-
  * syntax: datatype[]
  */
+const myNames = ['Razu', 'rakib', 77, , null];
 const names = ['Razu', 'rakib', 'rajib', 'sakib'];
 const numbers = [10, 2, 1, 25, 16];
-names.map((value) => {
-    console.log(value);
-});
+// names.map((value) => {
+//     console.log(value);
+// })
+/**
+ * tuple
+ * const variable_name: [type,type]= [value, value]
+ */
+let user = [10, 'Razu'];
+// user[0] = "abc"; // error
+user[0] = 12;
+const isMarid = ["Razu", false];
