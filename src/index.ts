bffd905146f2c27e002a194c6cf6a346e5bb4cca
razumolla <rajuahmed9344@gmail.com>
2023-05-01 10:56:07 +0600

    primitive data type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
 const course: string = "Next level web development 2 3";
-console.log(course);
+const rollNumber: number = 12;
+const isAdmin: boolean = true;
+
+let a = 12;
+
+let cour:null;
+let courr: undefined;
+let abc = undefined;
+const abcd = undefined;
+
+/**
+ * Primitive type 
+ * string
+ * number
+ * boolean
+ * null
+ * undefind
+ * 
+ */
