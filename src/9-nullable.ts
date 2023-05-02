a3dbeece999670489588c1555438c16e518bcfad
razumolla <rajuahmed9344@gmail.com>
2023-05-02 09:06:30 +0600

    null ,unknown and never type

---
+++ src/9-nullable.ts
@@ -0,0 +1,50 @@
+/**
+ * Null , Unknown and Never type
+ */
+
+const searchName = (value: string) => {
+    if (value === null) {
+        console.log("There is nothing to search");
+    }else {
+        console.log('Searching');    
+    }
+}
+// searchName("Razu")
+// searchName(null)
+
+/**
+ * Unknown type: type we can know - Runtime e  
+ * kmh^-1 --- ms^-1
+ */
+
+const getMyCarSpeed = (speed: unknown)=>{
+    if (typeof speed === 'number') {
+        const convertedSpeed = (speed * 1000) / 3600;
+        console.log(`My speed is ${convertedSpeed}`);
+    }
+    if (typeof speed === 'string') {
+        const [value,_unit] = speed.split(' ')
+        const convertedSpeed = (parseFloat(value) * 1000) / 3600;
+        console.log(`My speed is ${convertedSpeed}`);
+    } else {
+        console.log('There is wrong type');
+    }
+    
+}
+// getMyCarSpeed(10)
+// getMyCarSpeed('15 kmh^-1')
+// getMyCarSpeed(true)
+
+
+
+
+
+/**
+ * Never type
+ * Never return anything, only throw error with dynamic message
+ */
+
+function throwError(message: string): never {
+    throw new Error(message)
+}
+// throwError('Never return only errror message');
