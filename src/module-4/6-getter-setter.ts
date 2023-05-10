5550ee69f9db81a71255c534bb4e9d0b9f54805e
razumolla <rajuahmed9344@gmail.com>
2023-05-10 16:20:01 +0600

    gretter and setter

--- src/module-4/6-getter-setter.ts
+++ src/module-4/6-getter-setter.ts
@@ -1,33 +1,40 @@
-/**
- * Run: npx ts-node-dev --respawn src/module-4/6-getter-setter.ts
- */
+// /**
+//  * Run: npx ts-node-dev --respawn src/module-4/6-getter-setter.ts
+//  */
 
-class BankAccount {
-    public id: number;
-    public name: string;
-    // private _balance: number;
-    protected _balance: number;
-    constructor(id: number, name: string, balance: number) {
-        this.id = id;
-        this.name = name;
-        this._balance = balance;
+// class BankAccount {
+//     public id: number;
+//     public name: string;
+//     private _balance: number;
+//     constructor(id: number, name: string, balance: number) {
+//         this.id = id;
+//         this.name = name;
+//         this._balance = balance;
 
-    }
-    getBalance() {
-        console.log(`My current account is ${this._balance}`);
-    }
-    addDeposit(amount: number) {
-        this._balance = this._balance + amount;
-    }
-}
-
-class StudentAccount extends BankAccount{
-    test(){
-        // this._balance
-    }
-}
+//     }
+//     //gretter
+//     get balance(): number {
+//         return this._balance;
+//     }
+//     // getBalance() {
+//     //    return this._balance
+//     // }
 
 
-const myAccount = new BankAccount(1001, 'Razu', 500);
-// myAccount.balance = 0;
-console.log(myAccount);
+//     //setter
+//     set deposit(amount: number){
+//         this._balance = this._balance + amount;
+//     }
+//     // addDeposit(amount: number) {
+//     //     this._balance = this._balance + amount;
+//     // }
+// }
+
+
+// const myAccount = new BankAccount(1001, 'Razu', 500);
+
+// // myAccount.addDeposit(20);
+// // myAccount.getBalance();
+// console.log(myAccount.balance); //500
+// myAccount.deposit=100
+// console.log(myAccount.balance); //600
