2b3a0b104a079a7eba7660a65263ac407b20de5e
razumolla <rajuahmed9344@gmail.com>
2023-05-13 16:18:37 +0600

    null unknown

--- src/session/7-union-intersection.ts
+++ src/session/7-union-intersection.ts
@@ -1,31 +1,31 @@
-/**
- * union= allow all
- * intersection= allow only common numbers
- */
-type HeroTypeA = {
-    name: string;
-    age?: number;
-}
+// /**
+//  * union= allow all
+//  * intersection= allow only common numbers
+//  */
+// type HeroTypeA = {
+//     name: string;
+//     age?: number;
+// }
 
-type HeroTypeB = HeroTypeA & {
-    salary: number;
-    position?: string;
-}
+// type HeroTypeB = HeroTypeA & {
+//     salary: number;
+//     position?: string;
+// }
 
-const HeroData:HeroTypeB = {
-    name: "jalil",
-    age: 23,
-    salary: 200000,
-    position: "Developer"
-}
+// const HeroData:HeroTypeB = {
+//     name: "jalil",
+//     age: 23,
+//     salary: 200000,
+//     position: "Developer"
+// }
 
-//union -> |
-// intersection -> &
+// //union -> |
+// // intersection -> &
 
-const myFavHero:HeroTypeA & HeroTypeB = {
-    name: "khan",
-    age: 30,
-    salary: 34454,
-    // position: "Actor"
-}
+// const myFavHero:HeroTypeA & HeroTypeB = {
+//     name: "khan",
+//     age: 30,
+//     salary: 34454,
+//     // position: "Actor"
+// }
 
