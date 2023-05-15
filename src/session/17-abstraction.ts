f150824c5d7fd0fcd3f25796640fe1c4e0942db8
razumolla <rajuahmed9344@gmail.com>
2023-05-15 21:29:16 +0600

    type guard, polymorphism, abst, encap

---
+++ src/session/17-abstraction.ts
@@ -0,0 +1,22 @@
+/**
+ * abstraction :
+ * abstract class ke inherite kora jai but instance kora jai nah
+ */
+
+// abstract 
+abstract class Car{
+    abstract start(): void;
+    abstract stop(): void;
+}
+
+class MyCar extends Car{
+    start(): void {
+        console.log("Car Starting... ");
+    }
+    stop(): void {
+        console.log("Car stoped...");
+    }
+}
+
+const car1 = new MyCar();
+car1.start();
