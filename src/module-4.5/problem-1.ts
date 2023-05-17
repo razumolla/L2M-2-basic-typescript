7da70237fb1b841e05de66a08742012fb486d9ab
razumolla <rajuahmed9344@gmail.com>
2023-05-17 22:24:21 +0600

    problem task

---
+++ src/module-4.5/problem-1.ts
@@ -0,0 +1,15 @@
+/**
+ * Create a function that takes an array of numbers as input and returns the sum of all the numbers in the array. Make sure to define the types for the input parameter and return value.
+ */
+
+
+// function sumArray(numbers: number[]): number {
+//     let sum = 0;
+//     for (let num of numbers) {
+//       sum += num;
+//     }
+//     return sum;
+//   }
+//   const numbers: number[] = [1, 2, 3, 4, 5];
+//   const result: number = sumArray(numbers);
+//   console.log(result);  // Output: 15
