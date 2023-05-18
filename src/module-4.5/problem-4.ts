25d3234ce070eca147a75682c6ed7e06825325e9
razumolla <rajuahmed9344@gmail.com>
2023-05-18 23:32:51 +0600

    task problem

--- src/module-4.5/problem-4.ts
+++ src/module-4.5/problem-4.ts
@@ -4,12 +4,12 @@
  * 4. Implement a generic function called reverseArray that takes an array of any type and returns the reversed version of the array. Test it with different types of arrays (e.g., numbers, strings) to ensure it works correctly.
  */
 
-// function reverseArray<T>(array:T[]):T[] {
-//     return array.reverse();
-// }
-// const numbers: number[] = [1, 2, 3, 4, 5];
-// console.log(reverseArray(numbers));
+function reverseArray<T>(array:T[]):T[] {
+    return array.reverse();
+}
+const numbers: number[] = [1, 2, 3, 4, 5];
+console.log(reverseArray(numbers));
   
-//   const strings: string[] = ["apple", "banana", "cherry"];
-//   console.log(reverseArray(strings));  // Output: ["cherry", "banana", "apple"]
+  const strings: string[] = ["apple", "banana", "cherry"];
+  console.log(reverseArray(strings));  // Output: ["cherry", "banana", "apple"]
   
--- src/session/11-generic.ts
+++ src/session/11-generic.ts
@@ -1,84 +1,84 @@
-// /**
-//  * npx ts-node-dev --respawn src/session/10-generic.ts
-//  * -----------------------------------------------------
-//  * 
-//  */
+/**
+ * npx ts-node-dev --respawn src/session/10-generic.ts
+ * -----------------------------------------------------
+ * 
+ */
 
 
-// type myArray<T> = Array<T>
+type myArray<T> = Array<T>
 
-// // T = number
-// // Array<number>
+// T = number
+// Array<number>
 
-// const myNumbers: myArray<number> = [1, 2, 3, 4, 5];
-// const myStrings: myArray<string> = ['a', 'xyz', 'abc', 'pqr'];
-// const myBoolean: myArray<boolean> = [true, false, false, true];
+const myNumbers: myArray<number> = [1, 2, 3, 4, 5];
+const myStrings: myArray<string> = ['a', 'xyz', 'abc', 'pqr'];
+const myBoolean: myArray<boolean> = [true, false, false, true];
 
-// type dataType = {name: string, age:number};
+type dataType = {name: string, age:number};
 
-// const ourInfo:myArray<dataType> = [
-//     {name: "fahim", age: 70},
-//     {name: "mir", age: 60},
-//     {name: "mezba", age: 50}
-// ]
-
-
-
-
-
-// // tuple in generic
-// type MyGenType<X, Y, Z> = [X, Y, Z];
-
-// const A: MyGenType<string, number, number> = ['hero', 34, 30];
-// const B: MyGenType<number, number, string> = [45, 34, 'abc'];
-// const C: MyGenType<number, string, boolean> = [45, 'khan', true];
-
-// // generic in function
-// const printInfo = <P, Q>(param1:P, param2:Q):P =>{
-//     console.log(`param1: ${param1}, param2: ${param2}`)
-//     return param1;
-// }
-
-// console.log(printInfo<string, number>('fahim', 32))
-// // printInfo<boolean, number>(true, 32)
-// // printInfo<boolean, string>(true, 'hello')
+const ourInfo:myArray<dataType> = [
+    {name: "fahim", age: 70},
+    {name: "mir", age: 60},
+    {name: "mezba", age: 50}
+]
 
 
 
 
 
+// tuple in generic
+type MyGenType<X, Y, Z> = [X, Y, Z];
 
-// // generic in interface
-// interface IMe<T, U> {
-//     name: string;
-//     age: T; // 23, 23years
-//     mark: U;
-// }
-// const me:IMe<string, number> = {
-//     name: "Firoz",
-//     age: "23 years",
-//     mark: 34 
-// }
+const A: MyGenType<string, number, number> = ['hero', 34, 30];
+const B: MyGenType<number, number, string> = [45, 34, 'abc'];
+const C: MyGenType<number, string, boolean> = [45, 'khan', true];
+
+// generic in function
+const printInfo = <P, Q>(param1:P, param2:Q):P =>{
+    console.log(`param1: ${param1}, param2: ${param2}`)
+    return param1;
+}
+
+console.log(printInfo<string, number>('fahim', 32))
+// printInfo<boolean, number>(true, 32)
+// printInfo<boolean, string>(true, 'hello')
 
 
 
 
 
-// // keyof----------------
-// type MovieType = {
-//     name: string;
-//     duration: number;
-//     category: string;
-// }
 
-// // type test = 'name' | 'duration' | 'category' //this prob we can solve using 
-// // const myTest:test = 'name'
-// const data:MovieType = {
-//     name: "Agni 2",
-//     duration: 310,
-//     category: "Action"
-// }
+// generic in interface
+interface IMe<T, U> {
+    name: string;
+    age: T; // 23, 23years
+    mark: U;
+}
+const me:IMe<string, number> = {
+    name: "Firoz",
+    age: "23 years",
+    mark: 34 
+}
 
-// type myMoviesType = keyof MovieType // 'name' | 'duration' | 'category'
-// const myText11:myMoviesType = 'name'
-// console.log(data[myText11]);
+
+
+
+
+// keyof----------------
+type MovieType = {
+    name: string;
+    duration: number;
+    category: string;
+}
+
+// type test = 'name' | 'duration' | 'category' //this prob we can solve using 
+// const myTest:test = 'name'
+const data:MovieType = {
+    name: "Agni 2",
+    duration: 310,
+    category: "Action"
+}
+
+type myMoviesType = keyof MovieType // 'name' | 'duration' | 'category'
+const myText11:myMoviesType = 'name'
+console.log(data[myText11]);
